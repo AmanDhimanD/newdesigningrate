18556d33c9b1e0fb4a0a4db11c0d833b830ce539
Dhiman_aman <dhiman.1999.aman@gmail.com>
2022-10-21 22:28:35 +0530

    NewUpdate

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,32 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 /* import CurrentExchangePrices from "../Components/CurrentExchangePrices"; */
 /* import Cards from "../Components/Cards"; */
 import CurrentExchangePriceClass from "../Components/CurrentExchangePriceClass";
+import { HashLoader } from "react-spinners";
 
 const Home = () => {
   const styleCSS = {
     color: "white",
   };
+  const loaderCSS = {
+    display: "Flex",
+    justifyContent: "center",
+  };
+  const [load, setload] = useState(false);
+
+  useEffect(() => {
+    setload(true);
+    setTimeout(() => {
+      setload(false);
+    }, 5000);
+  }, []);
+
   return (
     <>
       {/* <CurrentExchangePrices/> */}
       <div className="">
+        <h3 className="text-white text-center text-xl">Demo Text </h3>
         <div className="text-white mx-10">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            totam assumenda dolorem cum quam architecto nostrum ducimus itaque
-            porro, accusantium veritatis ea vero. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Molestiae totam assumenda dolorem cum
-            quam architecto nostrum ducimus itaque porro, accusantium veritatis
-            ea vero. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Molestiae totam assumenda dolorem cum quam architecto nostrum
-            ducimus itaque porro, accusantium veritatis ea vero. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Molestiae totam
-            assumenda dolorem cum quam architecto nostrum ducimus itaque porro,
-            accusantium veritatis ea vero.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          totam assumenda dolorem cum quam architecto nostrum ducimus itaque
+          porro, accusantium veritatis ea vero. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Molestiae totam assumenda dolorem cum
+          quam architecto nostrum ducimus itaque porro, accusantium veritatis ea
+          vero. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Molestiae totam assumenda dolorem cum quam architecto nostrum ducimus
+          itaque porro, accusantium veritatis ea vero. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Molestiae totam assumenda dolorem
+          cum quam architecto nostrum ducimus itaque porro, accusantium
+          veritatis ea vero.
         </div>
-        <div className="mt-10 mx-10">
-        <CurrentExchangePriceClass />
-        </div>
+        {load ? (
+          <HashLoader
+            color={"green"}
+            loading={load}
+            size={40}
+            style={loaderCSS}
+          />
+        ) : (
+          <div className="mt-10 mx-10">
+            <CurrentExchangePriceClass />
+          </div>
+        )}
       </div>
     </>
   );
